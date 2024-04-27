89e12d6448d58c639f726d4a787332e3b9a16742
Nicolas Molina <nicolasmolina0920@hotmail.com>
2024-04-27 16:57:39 -0300

    Actualizando mis archivos a tipo TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
-function App() {
+/* eslint-disable no-unused-vars */
 
+import Guitar from './components/Guitar.jsx';
+import Header from './components/Header.jsx';
+
+import { useCart } from './hooks/useCart.js';
+
+function App() {
+  const {
+    data,
+    cart,
+    addToCart,
+    decrementQuantity,
+    increaseQuantity,
+    clearCart,
+    removeToCart,
+    cartTotal,
+    isEmpty,
+  } = useCart();
 
   return (
     <>
-     <h1>TypeScript</h1>
+      <Header
+        cart={cart}
+        removeToCart={removeToCart}
+        increaseQuantity={increaseQuantity}
+        decrementQuantity={decrementQuantity}
+        clearCart={clearCart}
+        cartTotal={cartTotal}
+        isEmpty={isEmpty}
+      />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
+          ))}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
